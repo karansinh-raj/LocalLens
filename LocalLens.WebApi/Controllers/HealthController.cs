048264d6e3e70cb2718056fe6aa04c65b2057dc0
Akash Rana <akashrana.work@gmail.com>
2024-07-20 22:05:30 +0530

    - Add health API

---
+++ LocalLens.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalLens.WebApi.Controllers;
+
+[Route("health")]
+public class HealthController : BaseController
+{
+	[HttpGet]
+	public async Task<IActionResult> GetHealthAsync()
+	{
+		return await Task.FromResult(Ok());
+	}
+}
