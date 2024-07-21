8903b30828002953298e75911b19097032c05569
karansinh-raj <karanraj.work@gmail.com>
2024-07-21 07:45:19 +0530

    - Added Questions Order

---
+++ LocalLens.WebApi/Migrations/20240721015931_AddedQuestionsOrderColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalLens.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedQuestionsOrderColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionOrder",
+                table: "Questions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionOrder",
+                table: "Questions");
+        }
+    }
+}
